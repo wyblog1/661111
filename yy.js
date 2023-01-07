d0ff430f61b1af8c0d9876249e03a8441f372ad9
wuying <106313976+612901@users.noreply.github.com>
2023-01-07 15:55:33 +0800

    Create yy.js

---
+++ yy.js
@@ -0,0 +1,27 @@
+function player_onPlay(){
+  //音乐播放
+}
+function player_onPause(){
+  //音乐暂停,可能是用户手动或者放完了
+}
+function player_onEnded(){
+  //音乐放完了
+}
+function player_onWaiting(){
+  //加载中
+}
+function player_onError(){
+  //出错
+}
+function player_onSeeked(){
+  //进度条跳跃
+}
+function player_onVolumechange(){
+  //更改音量
+}
+function player_onProgress(){
+  //进度条加载
+}
+function player_onListSwitch(){
+  //换歌
+}
